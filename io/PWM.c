befa72a2b92e1e13434db9da8cdd6bbb4bdda6c1
etienneluigi <etienne.luigi@gmail.com>
2017-07-01 10:44:25 +0200

    created InputOutput folder and moved all drivers files in it

--- io/PWM.c
+++ io/PWM.c
@@ -12,33 +12,6 @@
 void PWM_Init()
 {
 
-	//__TIM3_CLK_ENABLE();
-
-	    /**TIM3 GPIO Configuration
-	    PA6     ------> TIM3_CH1
-	    */
-	/*
-	GPIO_InitStruct.Pin = GPIO_PIN_6;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	htim3.Instance = TIM3;
-	htim3.Init.Prescaler = 499;
-	htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim3.Init.Period = 999;
-	htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-
-	HAL_TIM_PWM_Init(&htim3);
-
-	OcConfig.OCMode = TIM_OCMODE_PWM1;
-	OcConfig.Pulse = 0;
-	OcConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
-	OcConfig.OCFastMode = TIM_OCFAST_DISABLE;
-	HAL_TIM_PWM_ConfigChannel(&htim3, &OcConfig, TIM_CHANNEL_1);
-*/
-
 	TIM_OC_InitTypeDef sConfigOC;
 
 	  htim3.Instance = TIM3;
